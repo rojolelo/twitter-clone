b4dc3a2c41674583b2fdf564ce898457096ba2e1
rojolelo <legonesronny@gmail.com>
2019-08-08 22:12:37 -0400

    Updates

---
+++ precache-manifest.ce942bbce1b85e7baab2b557c732fdd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37db09da57577969f4043ad9cf19e6cc",
+    "url": "/twitter-clone/index.html"
+  },
+  {
+    "revision": "9eee3795c791d46fa336",
+    "url": "/twitter-clone/static/css/main.3ef53d43.chunk.css"
+  },
+  {
+    "revision": "3e97e764d14a5a610aec",
+    "url": "/twitter-clone/static/js/2.a2e76fe9.chunk.js"
+  },
+  {
+    "revision": "9eee3795c791d46fa336",
+    "url": "/twitter-clone/static/js/main.10105466.chunk.js"
+  },
+  {
+    "revision": "c6dea580d01e901b4bc4",
+    "url": "/twitter-clone/static/js/runtime~main.d3dd3cd8.js"
+  }
+]);
